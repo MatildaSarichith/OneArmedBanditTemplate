49dc598157494e732a9d3f81e355f387d2784690
Matilda Sarichith <matisari676@ed.amdsb.ca>
2022-03-11 14:35:06 -0500

    more code

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -12,8 +12,10 @@ namespace OneArmedBandit
     public partial class Form1 : Form
     {
         // random number generator
+        Random randGen = new Random();
 
         // int value for score initialized to 10
+        int score = 10;
 
 
         public Form1()
@@ -24,18 +26,54 @@ namespace OneArmedBandit
         private void spinButton_Click(object sender, EventArgs e) 
         {
             // get random values for each reel (store each in separate int variable)
-
+            int reel1 = randGen.Next(1, 4);
+            int reel2 = randGen.Next(1, 4);
+            int reel3 = randGen.Next(1, 4);
 
             // check value of reel 1 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch (reel1)
+            {
+                case 1:
+                    pictureBox1.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             // check value of reel 2 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
+            switch (reel2)
+            {
+                case 1:
+                    pictureBox2.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox2.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox2.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             // check value of reel 3 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch (reel3)
+            {
+                case 1:
+                    pictureBox3.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox3.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox3.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
